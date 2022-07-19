f7d5a8dee29a76d5c3317ee5e40bd5570aebf788
kubstar <52975077+kubstar@users.noreply.github.com>
2022-07-19 16:44:31 +0530

    create (post) -- done

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -3,7 +3,29 @@ let userDb = require('../model/model');
 //api's
 //create & save, new user 
 exports.create = (req,res)=>{
-
+        // validate request
+        if(!req.body){
+            res.status(400).send({ message : "Content can not be emtpy!"});
+            return;
+        }
+    
+        // create new user
+        const user = new userDb({
+            name : req.body.name,
+            email : req.body.email,
+            gender: req.body.gender,
+            status : req.body.status
+        })
+    
+        // save user data in database
+        user.save(user).then(data => {
+                res.send(data)
+                // res.redirect('/add-user');
+            }).catch(err =>{
+                res.status(500).send({
+                message : err.message || "Some error occurred while creating a create operation"
+                });
+            });
 }
 
 //retrieve and return all users/same for single user too
