a46c901296b04fbcf73c34a11c3d9c1c6eb4684c
kubstar <52975077+kubstar@users.noreply.github.com>
2022-07-24 12:34:14 +0530

    add-user (frontend) -- init commit

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -20,8 +20,8 @@ exports.create = (req,res)=>{
     
         // save user data in database
         user.save(user).then(data => {
-                res.send(data)
-                // res.redirect('/add-user');
+                //res.send(data)
+                res.redirect('/add-user');
             }).catch(err =>{
                 res.status(500).send({
                 message : err.message || "Some error occurred while creating a create operation"
