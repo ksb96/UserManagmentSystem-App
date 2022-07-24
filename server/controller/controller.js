ec014cbad3ab1293c881e2b4aee17222bb7c45ac
kubstar <52975077+kubstar@users.noreply.github.com>
2022-07-24 15:49:33 +0530

    "create new user" -save -- done + jquery pop-up -- added

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -2,82 +2,84 @@ let userDb = require('../model/model');
 
 //api's
 //create & save, new user (POST)
-exports.create = (req,res)=>{
-        // validate request
-        if(!req.body){
-            res.status(400).send({ 
-                message : "Content can not be emtpy!"});
-            return;
-        }
-    
-        // create new user
-        const user = new userDb({
-            name : req.body.name,
-            email : req.body.email,
-            gender: req.body.gender,
-            status : req.body.status
-        })
-    
-        // save user data in database
-        user.save(user).then(data => {
-                //res.send(data)
-                res.redirect('/add-user');
-            }).catch(err =>{
-                res.status(500).send({
-                message : err.message || "Some error occurred while creating a create operation"
-                });
+exports.create = (req, res) => {
+    // validate request
+    if (!req.body) {
+        res.status(400).send({
+            message: "Content can not be emtpy!"
+        });
+        return;
+    }
+
+    // create new user
+    const user = new userDb({
+        name: req.body.name,
+        email: req.body.email,
+        gender: req.body.gender,
+        status: req.body.status
+    })
+    // save user in the database
+    user.save(user).then(data => {
+            res.send(data)
+          //res.redirect('/add-user');
+        }).catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while creating a create operation"
             });
+        });
 }
 
-//retrieve and return users/same for single user too (GET)
-exports.find = (req,res)=>{
-    if(req.query.id){
+// retrieve and return all users/ retrive and return a single user
+exports.find = (req, res) => {
+
+    if (req.query.id) {
         const id = req.query.id;
-    //single user 
-    userDb.findById(id).then(data =>{
-        if(!data){
-            res.status(404).send({
-                message: `User data not found!!!  ${id}`
+        userDb.findById(id).then(data => {
+                if (!data) {
+                    res.status(404).send({
+                        message: `User data not found!!! ${id}`
+                    })
+                } else {
+                    res.send(data)
+                }
             })
-        }else{
-            res.send(data)
-        }
-    }).catch(err=>{
-        res.status(500).send({
-            message: err.message || `Unexpected error from our end, tbh!!!  ${id}`
-        })
-    })
-    }else{
-    //find - all
-    userDb.find().then(user=>{
-        res.send(user)
-    }).catch(err=>{
-        res.status(500).send({
-            message:err.message || "It's not you its us, Sorry"
-        })
-    })
+            .catch(err => {
+                res.status(500).send({
+                    message: err.message || `Unexpected error from our end, tbh!! ${id}`
+                })
+            })
+    } else {
+        userDb.find().then(user => {
+                res.send(user)
+            }).catch(err => {
+                res.status(500).send({
+                    message: err.message || "It's not you its us, Sorry"
+                })
+            })
     }
 }
 
 //update the user with ID's (PUT)
-exports.update = (req,res)=>{
+exports.update = (req, res) => {
     //no data
-    if(!req.body){
+    if (!req.body) {
         return res.status(400).send({
             message: "data for update cannot be empty"
         })
     }
     //storing in var 'ID'
     const id = req.params.id;
-    userDb.findByIdAndUpdate(id, req.body, {useFindAndModify: false}).then(data=>{
-        if(!data){
+    userDb.findByIdAndUpdate(id, req.body, {
+        useFindAndModify: false
+    }).then(data => {
+        if (!data) {
             res.status(404).send({
                 message: `Cannot update user with id:${id}. Maybe user not found`
             })
-        }else{
+        } else {
             res.send(data)
         }
-    }).catch(err =>{
+    }).catch(err => {
         res.status(500).send({
             message: err.message || "It's not you its us, Sorry"
         })
@@ -85,19 +87,19 @@ exports.update = (req,res)=>{
 }
 
 //delete user with ID (DELETE)
-exports.delete = (req,res)=>{
+exports.delete = (req, res) => {
     const id = req.params.id;
     userDb.findByIdAndDelete(id).then(data => {
-            if(!data){
-                res.status(404).send({ 
-                    message : `Cannot Delete with id ${id}. Maybe id is wrong`})
-            }else{
+            if (!data) {
+                res.status(404).send({
+                    message: `Cannot Delete with id ${id}. Maybe id is wrong`
+                })
+            } else {
                 res.send({
-                    message : "User was deleted successfully!"
+                    message: "User was deleted successfully!"
                 })
             }
-        })
-        .catch(err =>{
+        }).catch(err => {
             res.status(500).send({
                 message: err.message || `Could not delete user with id = ${id}`
             });
